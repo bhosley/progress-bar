69774793140a954b41c5c2495f65f3fd1a66078f
bhosley <brandonscotthosley@gmail.com>
2024-08-17 22:43:26 -0400

    Add svg class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useParams, useSearchParams, BrowserRouter as Router, } from 'react-router-dom';
+
+import progress from './progress.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/progress-bar"
+          element={<Home />}
+        />
+        <Route
+          path="/progress-bar/:id"
+          element={<Test />}
+        />
+      </Routes>
+    </Router>
   );
 }
+
+function Home() {
+    return (
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+          <p>
+            <img src={progress} className='Progress-Bar' alt='bar'/>
+            <object type="image/svg+xml" data="button.svg?fill=red">
+                <param name="fill" value="blue" />
+                <param name="stroke" value="red" />
+            </object>
+          </p>
+        </header>
+        </div>
+    );
+}
+
+function Test() {
+    const { id } = useParams();
+    return (
+        <p>Test Element {id}</p>
+    );
+  }
 
 export default App;
